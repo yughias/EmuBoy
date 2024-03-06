c3d3b4f572d95e380a8f14506e5c70c381f8fcf1
sasal <sasa.laporta@gmail.com>
2024-03-06 22:08:45 +0100

    added joystick

--- src/joypad.c
+++ src/joypad.c
@@ -1,7 +1,9 @@
 #include <SDL2/SDL.h>
 #include "joypad.h"
 
+#define AXIS_DEAD_ZONE (1 << 13)
 #define JOYSTICK_CHECK(btn) (joystick && SDL_JoystickGetButton(joystick, SDL_CONTROLLER_BUTTON_ ## btn)) 
+#define NORM_AXIS(axis) if(axis > AXIS_DEAD_ZONE) axis = 1; else if(axis < -AXIS_DEAD_ZONE) axis = -1; else axis = 0
 
 uint8_t JOYP_REG;
 
@@ -19,6 +21,15 @@ void initJoypad(){
 void emulateJoypad(){
     uint8_t new_arrow_btn = 0x0F;
     uint8_t new_action_btn = 0x0F;
+    Sint16 x_axis = 0;
+    Sint16 y_axis = 0;
+
+    if(joystick){
+        x_axis = SDL_JoystickGetAxis(joystick, 0);
+        y_axis = SDL_JoystickGetAxis(joystick, 1);
+        NORM_AXIS(x_axis);
+        NORM_AXIS(y_axis);
+    }
 
     if(keystate[A_KEY] || JOYSTICK_CHECK(B))
         new_action_btn &= 0b1110;
@@ -38,16 +49,16 @@ void emulateJoypad(){
     if(keystate[START_KEY] || JOYSTICK_CHECK(Y))
         new_action_btn &= 0b0111;
 
-    if(keystate[RIGHT_KEY] || JOYSTICK_CHECK(DPAD_RIGHT))
+    if(keystate[RIGHT_KEY] || JOYSTICK_CHECK(DPAD_RIGHT) || x_axis == 1)
         new_arrow_btn &= 0b1110;
 
-    if(keystate[LEFT_KEY] || JOYSTICK_CHECK(DPAD_LEFT))
+    if(keystate[LEFT_KEY] || JOYSTICK_CHECK(DPAD_LEFT) || x_axis == -1)
         new_arrow_btn &= 0b1101;
     
-    if(keystate[UP_KEY] || JOYSTICK_CHECK(DPAD_UP))
+    if(keystate[UP_KEY] || JOYSTICK_CHECK(DPAD_UP) || y_axis == -1)
         new_arrow_btn &= 0b1011;
 
-    if(keystate[DOWN_KEY] || JOYSTICK_CHECK(DPAD_DOWN))
+    if(keystate[DOWN_KEY] || JOYSTICK_CHECK(DPAD_DOWN) || y_axis == 1)
         new_arrow_btn &= 0b0111;
 
     ARROW_BTN = new_arrow_btn;
